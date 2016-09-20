1b01c9ee9961556fc25db94a257c49a1998b0c56
Alessandro Zanardi <alessandro@zanardi.org>
2016-09-20 23:25:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// Highest word count (45 mins)
+
+// Write a function that given a string returns an array containing
+// the most frequent word in it. If the top frequency is shared by more than
+// one word, the array will contain all of them (the order is not relevant).
+// Your implementation should exclude anything that is not a word
+// (e.g. punctuation and numbers), and use lodash methods where possible.
+// If the input is an empty string, return an empty array.
+// If the input is not a string, an error should be thrown.
+// TIP: You'll need to use a regex.
+
+var _ = require('lodash');
+
+function highestWordCount (str) {
+
+}
+
+module.exports = highestWordCount;
